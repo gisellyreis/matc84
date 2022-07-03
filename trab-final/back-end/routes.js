2980351a2e7f6494c5d517bcd6210edcf9bd9ef2
regoncalvs <renatagoncalves04@gmail.com>
2022-07-03 20:32:48 -0300

    front-end todo list

--- trab-final/back-end/routes.js
+++ trab-final/back-end/routes.js
@@ -14,43 +14,46 @@ routes.get('/todos', (req, res) => {
 routes.post('/todo', (req, res) => {
     //const { id, tarefa } = req.body;
     try {
-    const data = fs.readFileSync('./db.json', 'utf8');
-    console.log(data);
-    try {
-        db = JSON.parse(data);
-        db.push( req.body );
-        console.log(db);
-        fs.writeFileSync('./db.json', JSON.stringify(db));
-        console.log('Salvo com sucesso');
-        return res.send(db);
+        const data = fs.readFileSync('./db.json', 'utf8');
+        console.log(data);
+        try {
+            db = JSON.parse(data);
+            db.push(req.body);
+            console.log(db);
+            fs.writeFileSync('./db.json', JSON.stringify(db));
+            console.log('Salvo com sucesso');
+            return res.send(db);
         } catch (err) {
-        console.error(err);
+            console.error(err);
         }
-    return
+        return
     } catch (err) {
-    console.error(err);
+        console.error(err);
     }
-  });
+});
 
 routes.delete('/todo/:id', (req, res) => {
-const { id } = req.params;
-db = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
-db.find((item, index) => {
-    console.log(item.id);
-    if (item.id == id) {
-        db.splice(index, 1);
-        fs.writeFileSync('./db.json', JSON.stringify(db));
-        return res.json(db);
-    }})
+    const { id } = req.params;
+    db = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+    db.find((item, index) => {
+        console.log(item.id);
+        if (item.id == id) {
+            db.splice(index, 1);
+            fs.writeFileSync('./db.json', JSON.stringify(db));
+            return res.json(db);
+        }
+    })
 });
 
 routes.put('/todo/:id', (req, res) => {
     const { id } = req.params;
-    const { tarefa } = req.body;
+    const { tarefa } = req.body.tarefa;
+    const { check } = req.body.check;
     db = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
     db.find((item, index) => {
         if (item.id == id) {
             db[index].tarefa = tarefa;
+            db[index].check = check;
             fs.writeFileSync('./db.json', JSON.stringify(db));
             return res.json(db);
         }
--- trab-final/front-end/src/App.js
+++ trab-final/front-end/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Task from './Task';
+
+axios.defaults.baseURL = 'http://localhost:3333';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    getTasks();    
+    }, [])
+  
+  const getTasks = async () => {
+    await axios.get('/todos')
+    .then(response => {
+      console.log(response.data)
+      setTasks(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  const newTask = async () => {
+    let maxId = parseInt(Math.max(...tasks.map(o => o.id))) + 1;
+    let task = {id: maxId, tarefa: '', check: false};
+    console.log(task);
+     setTasks(arr => [...arr, task]);
+     var data = task;
+     console.log(data);
+     var config = {
+         method: 'post',
+         url: `/todo`,
+         headers: {
+             'Content-Type': 'application/json'
+         },
+         data: data
+     };
+
+     await axios(config)
+         .then(response => {
+             console.log(response.data)
+         })
+         .catch((err) => {
+             console.log(err);
+         })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tasks" > 
+        {tasks.length > 0 ? tasks.map((e, i) => <Task data={e} key={e.id} />) : ''}
+      </div>
+      <button onClick={newTask}>Incluir Tarefa</button>
     </div>
   );
 }
